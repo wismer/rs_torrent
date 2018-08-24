7708a0a27e357bf147aa7f91fbd9047440a0306a
Matt Long <matthewhl@gmail.com>
2018-08-24 16:57:51 -0400

    add some file structure & add sha1 and uuid dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,70 +1,22 @@
 use std::net::{UdpSocket, SocketAddr, ToSocketAddrs};
-extern crate bip_bencode;
-extern crate hyper;
-
-
+use std::str;
 use std::default::Default;
-use bip_bencode::{BencodeRef, BRefAccess, BDecodeOpt};
 use std::io::{self, Write};
-use hyper::Client;
-use hyper::rt::{self, Future, Stream};
 use std::fs::{File};
 use std::io::prelude::*;
 
-const BLOCK_LENGTH: i64 = 16384;
+extern crate bip_bencode;
+extern crate hyper;
+extern crate sha1;
 
-struct TorrentFile {
-    pub length: i64,
-    pub path: Vec<String>
-}
+use bip_bencode::{BencodeRef, BRefAccess, BDecodeOpt};
+use hyper::Client;
+use hyper::rt::{self, Future, Stream};
+use sha1::{Sha1, Digest};
 
-struct Torrent {
-    pub files: Vec<TorrentFile>,
-    pub torrent_filename: String,
-    pub piece_length: i64,
-    pub pieces: Vec<Piece>,
-    // tracker_urls: Vec<String>
-}
 
-type Block = Option<Vec<u8>>;
-
-struct Piece {
-    pub blocks: Vec<Block>,
-    pub index_range: (usize, usize),
-    pub completed: bool
-}
 // 
 // fn extract_pieces()
-
-fn extract_file_info(bencode_ref: &BencodeRef) -> TorrentFile {
-    let file_length = bencode_ref.dict().unwrap().lookup(b"length").unwrap().int().unwrap();
-    let file_path = bencode_ref.dict().unwrap().lookup(b"path").unwrap().list().unwrap()
-        .clone()
-        .into_iter()
-        .map(|r| String::from(r.str().unwrap()))
-        .collect();
-
-    TorrentFile {
-        length: file_length,
-        path: file_path
-    }
-}
-
-
-fn create_empty_pieces(count: usize, piece_length: usize) -> Vec<Piece> {
-    let mut pieces: Vec<Piece> = Vec::with_capacity(count);
-    
-    for i in 0..count {
-        let piece: Piece = Piece {
-            blocks: vec![None; piece_length / BLOCK_LENGTH as usize],
-            index_range: (i * piece_length, (i * piece_length) + piece_length),
-            completed: false
-        };
-        pieces.push(piece);
-    }
-
-    pieces
-}
 
 fn main() {
     let mut torrent_file: File = File::open("./steven_universe.torrent").unwrap();
@@ -88,16 +40,16 @@ fn main() {
     let total_size = files
         .iter()
         .fold(0, |acc, ref file| acc + file.length);
-
+    let mut sh = Sha1::new();
     let sha_hash = info.dict().unwrap().lookup(b"pieces").unwrap().bytes().unwrap();
-    println!("SHA HASH {:?}", sha_hash.len());
-
+    sh.update(sha_hash);
     let piece_count = (total_size / piece_length) + 1; // this isn't exact. there could always be a payload that is exactly n * piece_length
     let mut torrent: Torrent = Torrent {
         files: files,
         torrent_filename: String::from(info.dict().unwrap().lookup(b"name").unwrap().str().unwrap()),
         piece_length: piece_length,
-        pieces: create_empty_pieces((total_size / piece_length) as usize, piece_count as usize)
+        pieces: create_empty_pieces((total_size / piece_length) as usize, piece_count as usize),
+        sha_hash: sh.digest().to_string()
     };
 
     let uri = "http://httpbin.org/ip".parse().unwrap();
