b5209285c8f76a2d44e128239d0b79fb81d93081
Matt Long <matthewhl@gmail.com>
2018-08-25 13:50:59 -0400

    more moving

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 pub mod torrent;
 
-use std::net::{UdpSocket, SocketAddr, ToSocketAddrs};
-use std::str;
+use std::net::{ToSocketAddrs};
 use std::default::Default;
 use std::io::{self, Write};
 use std::fs::{File};
@@ -9,15 +8,13 @@ use std::io::prelude::*;
 
 extern crate bip_bencode;
 extern crate hyper;
-extern crate sha1;
 
-use bip_bencode::{BencodeRef, BRefAccess, BDecodeOpt};
+use bip_bencode::{BencodeRef, BDecodeOpt};
 use hyper::Client;
 use hyper::rt::{self, Future, Stream};
-use sha1::{Sha1, Digest};
 
 
-use torrent::torrent::{TorrentFile};
+use torrent::torrent::{Torrent};
 // 
 // fn extract_pieces()
 /*
@@ -36,31 +33,7 @@ fn main() {
     torrent_file.read_to_end(&mut contents);
     // println!("CONTENTS: {:?}", contents);
     let torrent_content = BencodeRef::decode(&contents, BDecodeOpt::default()).unwrap();
-    let dict = torrent_content.dict().unwrap();
-    let announce_list = dict.lookup(b"announce-list").unwrap();
-    let info = dict.lookup(b"info").unwrap();
-    let files: Vec<TorrentFile> = info.dict().unwrap().lookup(b"files").unwrap()
-        .list().unwrap()
-        .clone()
-        .into_iter()
-        .map(|ref r| extract_file_info(r))
-        .collect();
-
-    let piece_length = info.dict().unwrap().lookup(b"piece length").unwrap().int().unwrap();
-    let total_size = files
-        .iter()
-        .fold(0, |acc, ref file| acc + file.length);
-    let mut sh = Sha1::new();
-    let sha_hash = info.dict().unwrap().lookup(b"pieces").unwrap().bytes().unwrap();
-    sh.update(sha_hash);
-    let piece_count = (total_size / piece_length) + 1; // this isn't exact. there could always be a payload that is exactly n * piece_length
-    let mut torrent: Torrent = Torrent {
-        files: files,
-        torrent_filename: String::from(info.dict().unwrap().lookup(b"name").unwrap().str().unwrap()),
-        piece_length: piece_length,
-        pieces: create_empty_pieces((total_size / piece_length) as usize, piece_count as usize),
-        sha_hash: sh.digest().to_string()
-    };
+    let torrent = Torrent::new(&torrent_content);
 
     let uri = "http://httpbin.org/ip".parse().unwrap();
     rt::run(fetch_url(uri));
--- src/torrent/torrent.rs
+++ src/torrent/torrent.rs
@@ -1,3 +1,9 @@
+extern crate sha1;
+extern crate bip_bencode;
+
+use self::sha1::{Sha1};
+use bip_bencode::{BencodeRef, BRefAccess};
+
 const BLOCK_LENGTH: i64 = 16384;
 
 pub struct TorrentFile {
@@ -23,6 +29,35 @@ pub struct Piece {
 }
 
 impl Torrent {
+    pub fn new(bencode_file: &BencodeRef) -> Torrent {
+        let dict = bencode_file.dict().unwrap();
+        let announce_list = dict.lookup(b"announce-list").unwrap();
+        let info = dict.lookup(b"info").unwrap();
+        let files: Vec<TorrentFile> = info.dict().unwrap().lookup(b"files").unwrap()
+            .list().unwrap()
+            .clone()
+            .into_iter()
+            .map(|ref r| extract_file_info(r))
+            .collect();
+
+        let piece_length = info.dict().unwrap().lookup(b"piece length").unwrap().int().unwrap();
+        let total_size = files
+            .iter()
+            .fold(0, |acc, ref file| acc + file.length);
+        let mut sh = Sha1::new();
+        let sha_hash = info.dict().unwrap().lookup(b"pieces").unwrap().bytes().unwrap();
+        sh.update(sha_hash);
+        let piece_count = (total_size / piece_length) + 1; // this isn't exact. there could always be a payload that is exactly n * piece_length
+
+        Torrent {
+            files: files,
+            torrent_filename: String::from(info.dict().unwrap().lookup(b"name").unwrap().str().unwrap()),
+            piece_length: piece_length,
+            pieces: create_empty_pieces((total_size / piece_length) as usize, piece_count as usize),
+            sha_hash: sh.digest().to_string()
+        }
+    }
+
     fn tracker_query_params(&self) -> Option<String> {
         
 
